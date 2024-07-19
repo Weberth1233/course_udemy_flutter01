b95d9ce959091025a7b711b35c4b61cd0caeebdf
Weberth1233 <weberth20-19@hotmail.com>
2024-07-19 10:32:14 -0300

    add file in folder datasources - clean archiceture

---
+++ lib/0_data/exceptions/exceptions.dart
@@ -0,0 +1,5 @@
+//Qaulquer erro que ocorra no servidor
+class ServerExceptions implements Exception {}
+
+//Qualquer erro que ocorra no cache
+class CacheExceptions implements Exception {}
---
+++ lib/0_data/repositories/advice_repository_impl.dart
@@ -0,0 +1,25 @@
+import 'package:advicer/0_data/exceptions/exceptions.dart';
+import 'package:advicer/1_domain/entities/advice_entity.dart';
+import 'package:advicer/1_domain/failures/failures.dart';
+import 'package:advicer/1_domain/repositories/advice_repository.dart';
+import 'package:dartz/dartz.dart';
+
+import '../datasources/advice_remote_datasources.dart';
+
+class AdviceRepositoryImpl implements AdviceRepository {
+  final AdviceRemoteDatasource adviceRemoteDatasource;
+
+  AdviceRepositoryImpl({required this.adviceRemoteDatasource});
+
+  @override
+  Future<Either<Failure, AdviceEntity>> getAdviceFromDatasource() async {
+    try {
+      final result = await adviceRemoteDatasource.getRandomAdviceFromApi();
+      return right(result);
+    } on ServerExceptions catch (_) {
+      return left(ServerFailure());
+    } catch (e) {
+      return left(GeneralFailure());
+    }
+  }
+}
